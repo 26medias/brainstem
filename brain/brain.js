b7bf8e3a3fce81561c633171609b2c6c7a01b9e7
Julien <julien@twenty-six-medias.com>
2020-06-02 19:09:32 -0400

    Base structure fixed

--- brain/brain.js
+++ brain/brain.js
@@ -1,16 +1,17 @@
 // Utilities
 var _			= require('underscore');
 var pstack		= require('pstack');
+var path		= require('path');
 
 // Nervous System
-var brainstem	= require('brainstem/brainstem');
-var cerebellum	= require('cerebellum/cerebellum');
-var cerebrum	= require('cerebrum/cerebrum');
-var glands		= require('glands/glands');
-var senses		= require('senses/senses');
-var memory		= require('memory/memory');
+var brainstem	= require(path.normalize(__dirname+'/brainstem/brainstem'));
+var cerebellum	= require(path.normalize(__dirname+'/cerebellum/cerebellum'));
+var cerebrum	= require(path.normalize(__dirname+'/cerebrum/cerebrum'));
+var glands		= require(path.normalize(__dirname+'/glands/glands'));
+var senses		= require(path.normalize(__dirname+'/senses/senses'));
+var memory		= require(path.normalize(__dirname+'/memory/memory'));
 
-var Brain = function {
+var Brain = function() {
 	this.brainstem	= brainstem(this);
 	this.cerebellum	= cerebellum(this);
 	this.cerebrum	= cerebrum(this);
@@ -20,7 +21,8 @@ var Brain = function {
 }
 
 Brain.prototype.init = function(identityName) {
-	this.memory.load_identity(identityName);
+	//this.memory.load_identity(identityName);
 }
 
-Brain.init();
+var brain = new Brain();
+brain.init();
--- brain/memory/memory.js
+++ brain/memory/memory.js
@@ -1,5 +1,11 @@
-var shortMemory		= require('short/shortMemory');
-var longMemory		= require('long/longMemory');
+// Utilities
+var _			= require('underscore');
+var pstack		= require('pstack');
+var path		= require('path');
+
+// Long & Short memory
+var shortMemory		= require(path.normalize(__dirname+'/short/shortMemory'));
+var longMemory		= require(path.normalize(__dirname+'/long/longMemory'));
 
 var memory = function(brain) {
 	this.brain			= brain;
